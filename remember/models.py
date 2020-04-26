b6c23df09fa603b6eef13d79fea9fe61909e4af2
DimaKalbfleysh <kalbfleysh@gmail.com>
2020-04-26 22:32:01 +0500

    Added Remember models

--- remember/models.py
+++ remember/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class City(models.Model):
+	name = models.CharField(
+		verbose_name="City name",
+		max_length=32,
+	)
+	people_population = models.PositiveIntegerField(
+		verbose_name="Population of people living in the city",
+		default=0,
+	)
+
+	def __str__(self):
+		return f"{self.name}"
+
+
+class Remember(models.Model):
+	user = models.ForeignKey(
+		User,
+		verbose_name="User own remember",
+		on_delete=models.CASCADE,
+	)
+	city = models.ForeignKey(
+		City,
+		verbose_name="City of visit",
+		on_delete=models.CASCADE,
+	)
+	place = models.CharField(
+		verbose_name="Place of visit",
+		max_length=512,
+	)
+	description = models.TextField(
+		verbose_name="Description of remember",
+		null=True,
+	)
+	date = models.DateField(
+		verbose_name="Date of visit"
+	)
+
+	def __str__(self):
+		return f"{self.city.name}, {self.place}"
