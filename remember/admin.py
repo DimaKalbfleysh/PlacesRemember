46f5c8313256dcb908f8b95d8b397fd3fefddd32
DimaKalbfleysh <kalbfleysh@gmail.com>
2020-05-02 21:41:11 +0500

    Added admin classes for City and Remember

--- remember/admin.py
+++ remember/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from remember import models
 
 # Register your models here.
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'people_population']
+    list_filter = ['name', 'people_population']
+    search_fields = ['name']
+
+
+class RememberAdmin(admin.ModelAdmin):
+    list_display = ['user', 'city', 'place', 'date']
+    list_filter = ['user', 'city', 'date']
+    search_fields = ['user', 'city', 'place', 'description']
+
+
+admin.site.register(models.City, CityAdmin)
+admin.site.register(models.Remember, RememberAdmin)
