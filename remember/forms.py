7130937619f1e6e910c6eec03e20599cb2767b7c
DimaKalbfleysh <kalbfleysh@gmail.com>
2020-05-03 00:47:13 +0500

    Added class to create remembers

---
+++ remember/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from remember import models
+
+
+class RememberForm(forms.ModelForm):
+    city = forms.ChoiceField(label='Выбрать город', required=True)
+    place = forms.CharField(max_length=512, label='Место', required=True)
+    description = forms.CharField(widget=forms.Textarea)
+    date = forms.DateField(widget=forms.SelectDateWidget, label='Дата', required=True)
+
+    class Meta:
+        model = models.Remember
+        exclude = ['user']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['city'].choices = self.get_cities_choice()
+
+    def get_cities_choice(self):
+        cities = models.City.objects.all().order_by('-people_population')
+        return (
+            (city.id, city.name) 
+            for city in cities
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        city_id = cleaned_data.pop('city')
+        city = models.City.objects.get(pk=city_id)
+        cleaned_data['city'] = city
+        return cleaned_data
--- remember/views.py
+++ remember/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse
 
-# Create your views here.
-def remember_list(request):
-	return render(request, 'remember/remember_list.html')
+from remember import models
+from remember import forms
+
+
+class RememberViewMixin():
+    form_class = forms.RememberForm
+    model = models.Remember
+
+
+class RememberList(RememberViewMixin, ListView):
+	template_name = "remember/remember_list.html"
+	context_object_name = "remembers"
+
+	def get_queryset(self):
+		user = self.request.user
+		remembers = self.model.objects.filter(
+			user=user
+		)
+		return remembers
+
+
+class RememberCreate(RememberViewMixin, CreateView):
+	template_name = "remember/remember_form.html"
+
+	def form_valid(self, form):
+	    form.instance.user = self.request.user
+	    return super(RememberCreate, self).form_valid(form)
+
+	def get_success_url(self):
+	    return reverse("remember_list")
