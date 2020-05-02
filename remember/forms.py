ff414fe4f080a7d62cb78585a745546abbceb0a4
DimaKalbfleysh <kalbfleysh@gmail.com>
2020-05-03 01:13:17 +0500

    Refactor  -> ""

--- remember/forms.py
+++ remember/forms.py
@@ -4,21 +4,21 @@ from remember import models
 
 
 class RememberForm(forms.ModelForm):
-    city = forms.ChoiceField(label='Выбрать город', required=True)
-    place = forms.CharField(max_length=512, label='Место', required=True)
-    description = forms.CharField(widget=forms.Textarea)
-    date = forms.DateField(widget=forms.SelectDateWidget, label='Дата', required=True)
+    city = forms.ChoiceField(label="Выбрать город", required=True)
+    place = forms.CharField(max_length=512, label="Место", required=True)
+    description = forms.CharField(widget=forms.Textarea, label="Описание", required=False)
+    date = forms.DateField(widget=forms.SelectDateWidget, label="Дата", required=True)
 
     class Meta:
         model = models.Remember
-        exclude = ['user']
+        exclude = ["user"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['city'].choices = self.get_cities_choice()
+        self.fields["city"].choices = self.get_cities_choice()
 
     def get_cities_choice(self):
-        cities = models.City.objects.all().order_by('-people_population')
+        cities = models.City.objects.all().order_by("-people_population")
         return (
             (city.id, city.name) 
             for city in cities
@@ -26,7 +26,7 @@ class RememberForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        city_id = cleaned_data.pop('city')
+        city_id = cleaned_data.pop("city")
         city = models.City.objects.get(pk=city_id)
-        cleaned_data['city'] = city
+        cleaned_data["city"] = city
         return cleaned_data
